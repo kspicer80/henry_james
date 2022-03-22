8799245b9e7e0bbffd138f1e66f1971ed3d94a21
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-03-22 08:04:23 -0500

    text and initial script

---
+++ james_playground.py
@@ -0,0 +1,34 @@
+import spacy
+nlp = spacy.load('en_core_web_lg')
+
+test_string = '''The story had held us, round the fire, sufficiently breathless, but except the obvious remark that it was gruesome, as, on Christmas Eve in an old house, a strange tale should essentially be, I remember no comment uttered till somebody happened to say that it was the only case he had met in which such a visitation had fallen on a child. The case, I may mention, was that of an apparition in just such an old house as had gathered us for the occasion—an appearance, of a dreadful kind, to a little boy sleeping in the room with his mother and waking her up in the terror of it; waking her not to dissipate his dread and soothe him to sleep again, but to encounter also, herself, before she had succeeded in doing so, the same sight that had shaken him. It was this observation that drew from Douglas—not immediately, but later in the evening—a reply that had the interesting consequence to which I call attention. Someone else told a story not particularly effective, which I saw he was not following. This I took for a sign that he had himself something to produce and that we should only have to wait. We waited in fact till two nights later; but that same evening, before we scattered, he brought out what was in his mind.'''
+
+simpler_string = '''I went to the stores to get the store and stores went and go rock and rocks'''
+
+counts = {}
+dict_ids = {}
+
+#split_dict = {}
+#for k, v in results:
+    #split_dict.setdefault(k, []).append(v)
+#print(split_dict)
+
+final_dictionary = {}
+counts = {}
+
+nlp_test_string = nlp(simpler_string)
+
+for token in nlp_test_string:
+    final_dictionary[token.i] = (token.text, token.lemma_)
+    #for i in range(len(nlp_test_string)):
+        #final_dictionary[i] = (word.text, word.lemma_)
+
+print(final_dictionary)
+        
+#print(final_dictionary)
+    
+    #for id_number, word in word_ids:
+        #lemma_dictionary[id] = word.text, word.lemma_
+#for item in word_ids:
+    #print(item)
+#print(lemma_dictionary)
