481d33cc149b452bd47e1af65ff931be67a313c0
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-04-06 06:19:36 -0500

    no stopwords csv

--- james.py
+++ james.py
@@ -2,13 +2,14 @@ from more_itertools import pairwise
 import pandas as pd
 import spacy
 nlp = spacy.load('en_core_web_lg')
+stopwords = nlp.Defaults.stop_words
 
 #test_string = '''The story had held us, round the fire, sufficiently breathless, but except the obvious remark that it was gruesome, as, on Christmas Eve in an old house, a strange tale should essentially be, I remember no comment uttered till somebody happened to say that it was the only case he had met in which such a visitation had fallen on a child. The case, I may mention, was that of an apparition in just such an old house as had gathered us for the occasion—an appearance, of a dreadful kind, to a little boy sleeping in the room with his mother and waking her up in the terror of it; waking her not to dissipate his dread and soothe him to sleep again, but to encounter also, herself, before she had succeeded in doing so, the same sight that had shaken him. It was this observation that drew from Douglas—not immediately, but later in the evening—a reply that had the interesting consequence to which I call attention. Someone else told a story not particularly effective, which I saw he was not following. This I took for a sign that he had himself something to produce and that we should only have to wait. We waited in fact till two nights later; but that same evening, before we scattered, he brought out what was in his mind.'''
 
 # Just a silly string to test the lemmatizing that we need here ...
 #simpler_string = '''I went to the stores to get the store and stores went and go rock and rocks and rocks and rocked and rocking and stores and storing.'''
 
-with open('209-0-1.txt') as f:
+with open('209-0.txt') as f:
     data = f.read()
 
 spacy_text = nlp(data)
@@ -23,7 +24,7 @@ def find_repetitions(text):
     lemmas = []
     dictOfElems = dict()
     index = 0
-    text_no_punct = [token for token in spacy_text if not token.is_punct]
+    text_no_punct = [token for token in spacy_text if not token.is_punct and token not in stopwords]
     for token in text_no_punct:
         lemmas.append(token.lemma_)
     for elem in lemmas:
@@ -58,4 +59,4 @@ df = df.T
 df = df.rename(columns={0: 'number_of_repetitions', 1: 'indices'})
 print(df.head(10))
 #print(df.columns)
-df.to_csv('repetition_counts.csv')
+df.to_csv('repetition_counts_no_stopwords.csv')
