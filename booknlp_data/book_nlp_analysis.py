9d357cf9a0de16bf8eb4637e311fcd6b509d3323
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-04-08 16:21:02 -0500

    Update book_nlp_analysis.py

--- booknlp_data/book_nlp_analysis.py
+++ booknlp_data/book_nlp_analysis.py
@@ -3,108 +3,127 @@ from collections import Counter
 import pandas as pd
 pd.set_option('display.max_rows', None)
 
-df_entities = pd.read_csv(r"booknlp_data\turn_of_the_screw.entities", delimiter="\t")
-print(df_entities.head(200))
-print(df_entities['text'].unique())
+# Introductory Matters
 
-def process(filename):
-    with open(filename) as f:
-        data = json.load(f)
-    return data
-    
-def get_counter_from_dependency_list(dep_list):
-    counter = Counter()
-    for token in dep_list:
-        term = token['w']
-        tokenGlobalIndex = token['i']
-        counter[term] += 1
-    return counter
+with open('turn_of_the_screw.book', 'r') as f:
+    book_data = json.load(f)
+print(book_data.keys())
+print(len(book_data['characters']))
+print(book_data['characters'][0].keys())
+print(book_data['characters'][1]['mentions']['proper'])
+print(book_data['characters'][1]['mentions']['pronoun'])
 
-data = process(r'booknlp_data\turn_of_the_screw.book')
 
-def create_character_data(data, printTop):
-    character_data = dict()
-    for character in data['characters']:
-        agentList = character['agent']
-        patientList = character['patient']
-        possList = character['poss']
-        modList = character['mod']
-        character_id = character['id']
-        count = character['count']
-        referential_gender_distribution = referential_gender_prediction = 'unknown'
-        
-        if character['g'] is not None and character['g'] != 'unknown':
-            referential_gender_distribution = character['g']['inference']
-            referential_gender = character['g']['argmax']
-        
-        mentions = character['mentions']
-        proper_mentions = mentions['proper']
-        max_proper_mention = ''
-        
-        poss_items = []
-        agent_items = []
-        patient_items = []
-        mod_items = []
-        
-        if len(mentions['proper']) > 0:
-            max_proper_mention = mentions['proper'][0]['n']
-            
-            for k, v in get_counter_from_dependency_list(possList).most_common(printTop):
-                poss_items.append((v, k))
-            
-            for k, v in get_counter_from_dependency_list(agentList).most_common(printTop):
-                agent_items.append((v, k))
-                
-            for k, v in get_counter_from_dependency_list(patientList).most_common(printTop):
-                patient_items.append((v, k))
-        
-            for k, v in get_counter_from_dependency_list(modList).most_common(printTop):
-                mod_items.append((v, k))
-                
-            character_data[character_id] = {
-                                    'id': character_id,
-                                    'max_proper_mention': max_proper_mention,
-                                    'referential_gender': referential_gender,
-                                    'possList': poss_items,
-                                    'agentList': agent_items,
-                                    'patientList': patient_items,
-                                    'modList': mod_items
-                                    }
-    return character_data
 
-character_data = create_character_data(data, 10)
-#print(character_data)
-        
-def find_verb_usage(data, analysis = ['agent', 'patient']):
-    verb_analysis = []
-    for item in analysis:
-        if item == 'agent':
-            verb_analysis.append('agentList')
-        elif item == 'patient':
-            verb_analysis.append('patientList')
-    main_agents = {}
-    main_patients = {}
-    for character in character_data:
-        temp_data = character_data[character]
-        for item in verb_analysis:
-            for verb in temp_data[item]:
-                verb = verb[1].lower()
-                if item == 'agentList':
-                    if verb not in main_agents:
-                        main_agents[verb] = [(character, temp_data['max_proper_mention'])]
-                    else:
-                        main_agents[verb].append((character, temp_data['max_proper_mention']))
-                elif item == 'patientList':
-                    if verb not in main_patients:
-                        main_patients[verb] = [(character, temp_data['max_proper_mention'])]
-                    else:
-                        main_patients[verb].append((character, temp_data['max_proper_mention']))
-    verb_usage = {'agent': main_agents, 'patient': main_patients}
-    return verb_usage            
-            
-verb_data = find_verb_usage(data)
-print(verb_data['agent']['said'])
-    
+
+
+
+
+# Character Analysis
+
+#df_entities = pd.read_csv(r"booknlp_data\turn_of_the_screw.entities", delimiter="\t")
+#print(df_entities.head(200))
+#print(df_entities['text'].unique())
+#
+#def process(filename):
+    #with open(filename) as f:
+        #data = json.load(f)
+    #return data
+#
+#def get_counter_from_dependency_list(dep_list):
+    #counter = Counter()
+    #for token in dep_list:
+        #term = token['w']
+        #tokenGlobalIndex = token['i']
+        #counter[term] += 1
+    #return counter
+#
+#data = process(r'booknlp_data\turn_of_the_screw.book')
+#
+#def create_character_data(data, printTop):
+    #character_data = dict()
+    #for character in data['characters']:
+        #agentList = character['agent']
+        #patientList = character['patient']
+        #possList = character['poss']
+        #modList = character['mod']
+        #character_id = character['id']
+        #count = character['count']
+        #referential_gender_distribution = referential_gender_prediction = 'unknown'
+#
+        #if character['g'] is not None and character['g'] != 'unknown':
+            #referential_gender_distribution = character['g']['inference']
+            #referential_gender = character['g']['argmax']
+#
+        #mentions = character['mentions']
+        #proper_mentions = mentions['proper']
+        #max_proper_mention = ''
+#
+        #poss_items = []
+        #agent_items = []
+        #patient_items = []
+        #mod_items = []
+#
+        #if len(mentions['proper']) > 0:
+            #max_proper_mention = mentions['proper'][0]['n']
+#
+            #for k, v in get_counter_from_dependency_list(possList).most_common(printTop):
+                #poss_items.append((v, k))
+#
+            #for k, v in get_counter_from_dependency_list(agentList).most_common(printTop):
+                #agent_items.append((v, k))
+#
+            #for k, v in get_counter_from_dependency_list(patientList).most_common(printTop):
+                #patient_items.append((v, k))
+#
+            #for k, v in get_counter_from_dependency_list(modList).most_common(printTop):
+                #mod_items.append((v, k))
+#
+            #character_data[character_id] = {
+                                    #'id': character_id,
+                                    #'max_proper_mention': max_proper_mention,
+                                    #'referential_gender': referential_gender,
+                                    #'possList': poss_items,
+                                    #'agentList': agent_items,
+                                    #'patientList': patient_items,
+                                    #'modList': mod_items
+                                    #}
+    #return character_data
+#
+#character_data = create_character_data(data, 10)
+##print(character_data)
+#
+#def find_verb_usage(data, analysis = ['agent', 'patient']):
+    #verb_analysis = []
+    #for item in analysis:
+        #if item == 'agent':
+            #verb_analysis.append('agentList')
+        #elif item == 'patient':
+            #verb_analysis.append('patientList')
+    #main_agents = {}
+    #main_patients = {}
+    #for character in character_data:
+        #temp_data = character_data[character]
+        #for item in verb_analysis:
+            #for verb in temp_data[item]:
+                #verb = verb[1].lower()
+                #if item == 'agentList':
+                    #if verb not in main_agents:
+                        #main_agents[verb] = [(character, temp_data['max_proper_mention'])]
+                    #else:
+                        #main_agents[verb].append((character, temp_data['max_proper_mention']))
+                #elif item == 'patientList':
+                    #if verb not in main_patients:
+                        #main_patients[verb] = [(character, temp_data['max_proper_mention'])]
+                    #else:
+                        #main_patients[verb].append((character, temp_data['max_proper_mention']))
+    #verb_usage = {'agent': main_agents, 'patient': main_patients}
+    #return verb_usage
+#
+#verb_data = find_verb_usage(data)
+#print(verb_data['agent']['said'])
+
+# Event Analysis
 
 #df = pd.read_csv('turn_of_the_screw.tokens', delimiter='\t')
 ##print(df.head())
