907f3e882b596bdc2d8cace254002cd4a041daa4
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-04-13 15:16:59 -0500

    Update word_sense_vectors.py

--- word_sense_vectors.py
+++ word_sense_vectors.py
@@ -6,7 +6,7 @@ nlp = spacy.load('en_core_web_lg')
 with open('209-0.txt', encoding='utf-8') as f:
     data = f.read()
 
-senseDocs = [nlp(w) for w in ['sight']]
+senseDocs = [nlp(w) for w in ['sight', 'sound', 'touch', 'smell']]
 
 def whichSense(word):
     doc = nlp(word)
@@ -16,10 +16,11 @@ nlp_of_file = nlp(data)
 #dictionary = {whichSense(w.text) for w in testWords}
 
 df = pd.DataFrame([whichSense(w.text) for w in nlp_of_file], index=nlp_of_file)
-df.columns = ['sight']
-df = df.sort_values(by=['sight'], ascending=False)
-print(df.dtypes)
-print(df.head(100))
+df.columns = ['sight', 'sound', 'touch', 'smell']
+df = df.sort_values(by=['sight', 'sound'], ascending=[False, False])
+df.plot(kind='bar')
+#print(df.head(100))
+df.to_csv('sense_test.csv')
 
 #pd.DataFrame(data=[*dict.values()], columns=['firstcolumn','secondcolumn', 'thirdcolumn'])
 
