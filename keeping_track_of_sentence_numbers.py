35409aa1c720dfac5b3b38d921e554c73aec8697
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-04-15 07:51:19 -0500

    sentence indices

--- keeping_track_of_sentence_numbers.py
+++ keeping_track_of_sentence_numbers.py
@@ -29,17 +29,29 @@ def find_repetitions(text):
             index += 1
     dictOfElems = {key: value for key, value in dictOfElems.items() if value[0]>1}
     return dictOfElems
-    
-result = find_repetitions(test_string)
-print(result)
 
-df = pd.DataFrame(result)
+#result = find_repetitions(test_string)
+#print(result)
+
+#df = pd.DataFrame(result)
 #print(df.head(50))
 #print(df.columns)
-df = df.T
+#df = df.T
 #print(df.head(25))
 #df = df.unstack(level=0)
 #df.columns = df.columns.droplevel()
 #df = df.rename(columns={0: 'number_of_repetitions', 1: 'indices'})
-print(df)
+#print(df)
 #df.to_csv('with_sentence_indexing.csv')
+
+test_dict = {}
+spacy_text = nlp(test_string)
+for sent_index, sent in enumerate(spacy_text.sents):
+    for token in sent:
+        print(sent_index, token.i - sent.start, token.text)
+
+# Keeping track of sentence numbers ...? https://stackoverflow.com/questions/50742516/how-to-get-the-index-of-a-token-in-a-sentence-in-spacy
+#for sent_index, sentence in enumerate(spacy_text.sents):
+    #for token in sentence:
+        #test_dict[sent_index] = (token.text, token.i - sentence.start)
+#print(test_dict)
