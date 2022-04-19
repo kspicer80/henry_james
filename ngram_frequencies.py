da92bca7920efb237268ad79470e9b4e34e1185f
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-04-19 06:40:03 -0500

    Update ngram_frequencies.py

--- ngram_frequencies.py
+++ ngram_frequencies.py
@@ -1,3 +1,4 @@
+import string
 import nltk
 from nltk import word_tokenize
 from nltk import WordNetLemmatizer
@@ -13,19 +14,23 @@ spacy_stopwords = nlp.Defaults.stop_words
 
 with open('209-0.txt', encoding='utf-8') as f:
     data = f.read()
+    data = data.translate(str.maketrans('','',string.punctuation))
 
 def compute_freq(sentence, n_value=4):
     tokens = nltk.word_tokenize(sentence)
-    ngrams = nltk.ngrams(tokens, n_value)
+    new_words = [token for token in tokens if token.isalnum()]
+    ngrams = nltk.ngrams(new_words, n_value)
     ngram_fdist = nltk.FreqDist(ngrams)
     return ngram_fdist
 
 four_gram_frequencies = compute_freq(data)
-print(four_gram_frequencies)
+print(four_gram_frequencies.keys())
 
 dictionary = {k: v for k, v in sorted(four_gram_frequencies.items(), key=lambda item: item[1])}
 #for k, v in four_gram_frequencies.items():
     #print(k, v)
-    
+
 df = pd.DataFrame.from_dict(dictionary, orient='index')
-print(df.head(50))
+df = df.sort_values(by=[0], ascending=False)
+#print(df.shape)
+#print(df.head(50))
