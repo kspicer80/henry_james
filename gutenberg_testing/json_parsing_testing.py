816fd203659cd8593cb8fe7c32a549947e8065d4
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-05-05 20:47:16 -0500

    iterating over the entries I want

--- gutenberg_testing/json_parsing_testing.py
+++ gutenberg_testing/json_parsing_testing.py
@@ -13,14 +13,24 @@ import json
 
 with open(r'C:\Users\KSpicer\Documents\GitHub\henry_james\gutenberg_testing\henry_james_gutenberg_ids.json', 'r') as f:
     data = json.load(f)
-print(data)
-#print(type(data))
+#print(data)
+##print(type(data))
+#
+##print(data['results'][0]['id'])
+##print(data['results'][1]['id'])
+#
+#for i in range(len(data['results'])):
+    #print(data['results'][i]['id'], data['results'][i]['title'])
+#
+#print(len(data['results']))
 
-#print(data['results'][0]['id'])
-#print(data['results'][1]['id'])
-
-for i in range(len(data['results'])):
-    print(data['results'][i]['id'], data['results'][i]['title'])
+for item in data['results']:
+    id = item['id']
+    title = item['title']
+    print(id, title)
     
+
+
+
 #new_string = json.dumps(data, indent=2)
 #print(new_string)
