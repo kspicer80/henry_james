f0bdef461d704ee1b96787a28c9fb47ca4ba9f67
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-05-05 20:36:33 -0500

    updates

--- gutenberg_testing/json_parsing_testing.py
+++ gutenberg_testing/json_parsing_testing.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import json
-import os
+
 
 #path = Path('henry_james_gutenberg_ids.json')
 #print(path.exists())
@@ -11,19 +11,16 @@ import os
 #f.close()
 #print(data)
 
-#with open(r'C:\Users\KSpicer\Documents\GitHub\henry_james\gutenberg_testing\henry_james_gutenberg_ids.json', 'r') as f:
-    #data = json.load(f)
-#print(data)
+with open(r'C:\Users\KSpicer\Documents\GitHub\henry_james\gutenberg_testing\henry_james_gutenberg_ids.json', 'r') as f:
+    data = json.load(f)
+print(data)
 #print(type(data))
 
 #print(data['results'][0]['id'])
 #print(data['results'][1]['id'])
 
-#for i in range(len(data['results'])):
-    #print(data['results'][i]['id'])
+for i in range(len(data['results'])):
+    print(data['results'][i]['id'], data['results'][i]['title'])
     
-#new_string = json.dumps(data, indent=2, sort_keys=True)
+#new_string = json.dumps(data, indent=2)
 #print(new_string)
-print(os.getcwd())
-with open('henry_james_gutenberg_ids.json') as f:
-    data = json.load(f)
